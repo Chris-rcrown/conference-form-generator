56173c67d9888ecb34b558e9ef1708e4dacbd3eb
Rotimi Crown <rotimiworkmail1@gmail.com>
2025-02-15 16:47:24 +0100

    view height for mobile

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import TicketSelection from "./ticket-selection"
 const Home = () => {
     
   return (
-    <div className="flex flex-col items-center bg-[#02191D] h-[100vh] ">
+    <div className="flex flex-col items-center bg-[#02191D] h-auto md:h-[100vh] ">
       <Header/>
     <TicketSelection/> 
     </div>
