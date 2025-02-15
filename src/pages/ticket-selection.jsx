041d1b34435283b914116132c9fbec53fdcf5f51
Rotimi Crown <rotimiworkmail1@gmail.com>
2025-02-15 16:27:58 +0100

    corrected the height of the home page properly

--- src/pages/ticket-selection.jsx
+++ src/pages/ticket-selection.jsx
@@ -26,7 +26,7 @@ const TicketSelection = () => {
 
   return (
     <div className="flex justify-center p-6 ">
-      <Form className="bg-[#041E23] p-6 md:p-12 w-full max-w-2xl border border-[#0E464F] rounded-lg h-[100vh]">
+      <Form className="bg-[#041E23] p-6 md:p-12 w-full max-w-2xl border border-[#0E464F] rounded-lg ">
         
         {/* Header */}
         <div className="mb-6 border-b-2 border-[#0E464F]  flex flex-col md:flex-row md:justify-between md:items-center ">
