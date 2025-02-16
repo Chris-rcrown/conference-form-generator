227ff3187c673017fdcc0402f66720b527735ccb
Rotimi Crown <rotimiworkmail1@gmail.com>
2025-02-16 15:11:04 +0100

    ticket design

--- src/pages/getTicket.jsx
+++ src/pages/getTicket.jsx
@@ -47,7 +47,7 @@ const GetTicket = () => {
           </div>
           {/* Ticket card */}
           <div id="ticket-download" style={{fontFamily: 'robotor'}} className="bg-gradient-to-b from-[#133D44] to-[#031E21] p-4 relative custom-clip-path border border-[#24A0B5] rounded-2xl shadow-md mb-[24px] w-[300px] h-[700px] flex flex-col">
-          <div className="border border-[#24A0B5] rounded-2xl">
+            <div className="border border-[#24A0B5] rounded-2xl w-[260px] h-[446px] p-[14px] ">
             <div className="flex flex-col items-center w-[260px] h-[446px] py-4">
               <div className="mt-[14px]">
                 <h3 style={{fontFamily: 'Roadrage'}} className="text-lg  text-center text-gray-100 text-[34px]">
@@ -104,12 +104,14 @@ const GetTicket = () => {
                     {text || "No special requests"}
                   </span>
                 </div>
+                </div>
               </div>
-              </div>
+            </div>
+            <div className="bg-gradient-to-b from-[#133D44] to-[#031E21] p-4 relative custom-clip-path rounded-2xl">
               <div>
                 <LineDivider />
               </div>
-              <div className="mt-1 flex flex-col items-center justify-center h-[94px]">
+              <div className="mt-1 flex flex-col items-center justify-center h-[94px] ">
                 
                 <div className="flex">
                 <Barcode className="w-auto h-[50px] text-white"/>
@@ -122,8 +124,8 @@ const GetTicket = () => {
                   <span>891026</span>
                 </p>
               </div>
-            </div>
-        </div> 
+              </div>
+          </div> 
           {/* ticket end */}
           </div>
           <div style={{fontFamily: 'Jeju'}} className="flex flex-col md:flex-row gap-[24px] mt-[32px]">
