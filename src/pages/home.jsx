cb4ca42e2865a32e81330c39f7f412319de8a819
Rotimi Crown <rotimiworkmail1@gmail.com>
2025-02-16 13:43:45 +0100

    reverted

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import TicketSelection from "./ticket-selection"
 const Home = () => {
     
   return (
-    <div className="flex flex-col items-center bg-[#02191D]  lg:h-[100vh] ">
+    <div className="flex flex-col items-center bg-[#02191D]  lg:min-h-[100vh] ">
       <Header/>
     <TicketSelection /> 
     </div>
